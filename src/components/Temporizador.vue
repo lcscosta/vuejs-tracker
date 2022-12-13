9b0006608ae4e150ce461405112bcacc6cf1c38d
lcscosta <lcscosta@disroot.org>
2022-12-13 00:48:15 -0300

    Adicionado o comportamento aos botões

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
-    <button class="button" @click="iniciar">
+    <button class="button" @click="iniciar" :disabled="cronometroRodando">
       <span class="icon">
         <i class="fas fa-play"></i>
       </span>
       <span>play</span>
     </button>
-    <button class="button" @click="finalizar">
+    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
       <span class="icon">
         <i class="fas fa-stop"></i>
       </span>
@@ -28,19 +28,23 @@
     data() {
       return {
           tempoEmSegundos: 0,
-          cronometro: 0
+          cronometro: 0,
+          cronometroRodando: false,
+
       }
     },
     methods: {
       iniciar() {
         console.log('iniciou')
+        this.cronometroRodando = true;
         this.cronometro = setInterval(() => {
             this.tempoEmSegundos += 1;
         }, 1000)
       },
       finalizar() {
-          console.log('finalizou')
-          clearInterval(this.cronometro)
+        console.log('finalizou');
+        this.cronometroRodando = false;
+        clearInterval(this.cronometro);
       }
     }
   });
