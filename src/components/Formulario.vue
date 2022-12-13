fdf09a1d391b342a3aff6df84a8626e8df2f1614
lcscosta <lcscosta@disroot.org>
2022-12-13 00:36:45 -0300

    Refatorado para distribuir a responsabilidade

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -5,25 +5,7 @@
         <input type="text" class="input" placeholder="Qual tarefa iniciar?"/>
       </div>
       <div class="column">
-        <div class="is-flex is-align-items-center is-justify-content-space-between">
-          <section>
-            <strong>
-              {{ tempoDecorrido }}
-            </strong>
-          </section>
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <Temporizador />
       </div>
     </div>
   </div>
@@ -31,31 +13,12 @@
 
 <script lang="ts">
   import { defineComponent } from 'vue';
+  import  Temporizador from './Temporizador.vue';
 
   export default defineComponent({
     name:'TodoForms',
-    data() {
-      return {
-          tempoEmSegundos: 0,
-          cronometro: 0
-      }
-    },
-    computed: {
-      tempoDecorrido () : string {
-        return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
-      }
-    },
-    methods: {
-      iniciar() {
-        console.log('iniciou')
-        this.cronometro = setInterval(() => {
-            this.tempoEmSegundos += 1;
-        }, 1000)
-      },
-      finalizar() {
-          console.log('finalizou')
-          clearInterval(this.cronometro)
-      }
+    components: {
+      Temporizador
     }
   });
 </script>
---
+++ src/components/Temporizador.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
+    <button class="button" @click="iniciar">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+     </button>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import Cronometro from './Cronometro.vue'
+
+  export default defineComponent ({
+    name: 'TemporizadorForms',
+    components: {
+        Cronometro
+    },
+    data() {
+      return {
+          tempoEmSegundos: 0,
+          cronometro: 0
+      }
+    },
+    methods: {
+      iniciar() {
+        console.log('iniciou')
+        this.cronometro = setInterval(() => {
+            this.tempoEmSegundos += 1;
+        }, 1000)
+      },
+      finalizar() {
+          console.log('finalizou')
+          clearInterval(this.cronometro)
+      }
+    }
+  });
+</script>
