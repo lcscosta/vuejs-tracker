9ca88ada90ec16aad8867fa25502da1464c2f57e
lcscosta <lcscosta@disroot.org>
2022-12-12 23:44:05 -0300

    Adicionado logo e Formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="box">
     <div class="columns">
-      <div class="columns is-8" role="form" aria-label="formulário para criação de tarefa">
+      <div class="column is-8" role="form" aria-label="Formulário para criação de tarefa">
         <input type="text" class="input" placeholder="Qual tarefa iniciar?"/>
       </div>
       <div class="column">
-        <section>
-          <strong>00:00:00</strong>
-        </section>
-        <button class="button">
-          <span class="icon">
-            <i class="fas fa-play"></i>
-          </span>
-          <span>play</span>
-        </button>
-        <button class="button">
-         <span class="icon">
-           <i class="fas fa-stop"></i>
-         </span>
-         <span>stop</span>
-       </button>
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <section>
+            <strong>00:00:00</strong>
+          </section>
+          <button class="button">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -29,6 +31,6 @@
   import { defineComponent } from 'vue';
 
   export default defineComponent({
-    name:'Formulario'
+    name:'TodoForms'
   });
 </script>
